2b23e07831881b6b8e9467b4cc15e2014974bea3
Iskander Sabirov <iskodisko@172.31.73.209-vpn.dhcp.yndx.net>
2025-12-08 13:31:45 +0300

    time in script

--- utils/run_experiments.sh
+++ utils/run_experiments.sh
@@ -34,7 +34,7 @@ APP_PATH="${1:-./build/app}"
 OUTPUT_CSV="${2:-results.csv}"
 RUNS_PER_FILE="${3:-5}"
 PATH_TO_JSONS="${4:-./jsons}"
-TIME_LIMIT=10000
+TIME_LIMIT=50
 
 if [ ! -f "$APP_PATH" ]; then
     echo "Ошибка: приложение не найдено по пути $APP_PATH"
